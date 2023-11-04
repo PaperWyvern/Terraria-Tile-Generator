a13b77ac79303be86fe288a6fb7e461e94b0c43a
PaperWyvern <xBryton@gmail.com>
2023-11-03 20:00:25 -0700

    Update TTG.py

--- TTG.py
+++ TTG.py
@@ -1,9 +1,9 @@
 import random
-import PIL.Image
+from tkinter import *
+from tkinter.colorchooser import askcolor
+from PIL import Image
 import numpy as np
 import os
-from tkinter import *
-from tkinter.colorchooser import *
 import sys
 
 def resource_path(relative_path):
@@ -16,21 +16,20 @@ def resource_path(relative_path):
 
 root = Tk()
 
-shadesNum = StringVar()
+shadesNum = IntVar()
 texn = StringVar()
 textype = StringVar(value="TTG.png")
 global colors
-root.resizable(FALSE,FALSE)
+root.resizable(False, False)
 root.title("Terraria Tile Generator")
-    
+
 if not os.path.exists("Blocks"):
     os.makedirs("Blocks")
     print("Created dir 'Blocks'.")
 
 def MakeTile(shadesNum, colors):
     print("\nStarted!")
-    template = np.asarray(PIL.Image.open(resource_path(textype.get())).convert("RGBA"))
-    template.setflags(write=1)
+    template = np.array(Image.open(resource_path(textype.get())).convert("RGBA"))
     print(template.flags)
 
     for x in range(shadesNum):
@@ -48,7 +47,7 @@ def MakeTile(shadesNum, colors):
                 template[row + 1][pixel + 1] = randomColor
     print("Created image!")
 
-    template = PIL.Image.fromarray(np.asarray(template).astype('uint8'))
+    template = Image.fromarray(np.array(template).astype('uint8'))
     template.save('Blocks\\' + texn.get() + '.png')
     print("Saved image!")
     print("Done!")
@@ -58,11 +57,11 @@ def getColor():
     colorButton.configure(bg=color[1])
     hexcolor = color[1][1:]
     global colors
-    colors = [[int(hexcolor[0] + hexcolor[1], 16), int(hexcolor[2] + hexcolor[3], 16), int(hexcolor[4] + hexcolor[5], 16), 255]]
+    colors = [[int(hexcolor[0:2], 16), int(hexcolor[2:4], 16), int(hexcolor[4:6], 16), 255]]
 
 Label(root, text="Terraria Tile Generator", font='Helvetica 18 bold').grid(row=0, column=0, padx=10, pady=10, columnspan=2)
 Label(root, text="Number of lighter and darker shades:").grid(column=0, row=1, padx=10, pady=10)
-Entry(root, textvariable=shadesNum).grid(column=0, row = 2)
+Entry(root, textvariable=shadesNum).grid(column=0, row=2)
 Label(root, text="Enter Texture name:").grid(column=0, row=3, padx=10, pady=10)
 Label(root, text="Select Texture Type:").grid(column=1, row=3, padx=10, pady=10)
 Radiobutton(root, text="Jaggy-like Type", variable=textype, value="TTG.png").grid(column=1, row=4, padx=10)
@@ -70,7 +69,7 @@ Radiobutton(root, text="Smooth Type", variable=textype, value="TTGS.png").grid(c
 Entry(root, textvariable=texn).grid(column=0, row=4)
 Label(root, text="Pick a base color:").grid(column=1, row=1, padx=10, pady=10)
 colorButton = Button(root, command=getColor, text="                          ")
-colorButton.grid(column=1,row=2, padx=10, pady=10)
-Button(root, text="Generate!", command=lambda: MakeTile(int(shadesNum.get()), colors)).grid(row=5, column=0, padx=10, pady=10)
+colorButton.grid(column=1, row=2, padx=10, pady=10)
+Button(root, text="Generate!", command=lambda: MakeTile(shadesNum.get(), colors)).grid(row=5, column=0, padx=10, pady=10)
 
 root.mainloop()
